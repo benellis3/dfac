caacf2d792015fd84026a959e70bf29cb43c82d7
benellis3 <benellis@robots.ox.ac.uk>
2022-08-06 15:21:57 +0100

    First smac2 attempt

---
+++ run_docker.sh
@@ -0,0 +1,24 @@
+set -x
+HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+WANDB_API_KEY=$(cat $WANDB_API_KEY_FILE)
+GPU=$1
+name=${USER}_pymarl_GPU_${GPU}_${HASH}
+
+echo "Launching container named '${name}' on GPU '${GPU}'"
+# Launches a docker container using our image, and runs the provided command
+
+if hash nvidia-docker 2>/dev/null; then
+      cmd=nvidia-docker
+else
+      cmd=docker
+fi
+
+NV_GPU="$GPU" ${cmd} run -it \
+    -e WANDB_API_KEY=$WANDB_API_KEY \
+    --name $name \
+    --user $(id -u) \
+    --memory 100g \
+    -v $(pwd):/home/minsun/pymarl \
+    -v $(pwd)/results:/results \
+    -t pymarl:smacv2 \
+    ${@:2}
